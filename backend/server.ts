0ae6e316a445224e38bcc8ec77f39cfa54067515
s_murakami <s_murakami@geot.jp>
2022-06-07 08:53:31 +0900

    chore: Install colors module.

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,25 @@
 import express, { Application, Request, Response } from 'express'
+import colors from 'colors';
 
-const app: Application = express()
-const PORT = 5000
+const app: Application = express();
+const PORT = 5000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (_req: Request, res: Response) => {
   return res.status(200).send({
     message: 'Hello World!',
-  })
-})
+  });
+});
 
 try {
   app.listen(PORT, () => {
-    console.log(`dev server running at: http://localhost:${PORT}/`)
-  })
+    console.log(colors.yellow.bold(`dev server running at: http://localhost:${PORT}/`));
+  });
 } catch (e) {
   if (e instanceof Error) {
-    console.error(e.message)
+    console.error(e.message);
   }
 }
 
