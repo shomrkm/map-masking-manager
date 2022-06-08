fdeccf2962113214284f24f465c769af4326adc7
shomrkm <shochan1985@gmail.com>
2022-06-08 22:46:30 +0900

    fix: Modify writing of dotenv.

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,8 +1,10 @@
 import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config/.env' });
 
 const MONGO_URI: string = process.env.MONGO_URI as string;
 
 export const connectDB = async () => {
   const conn = await connect(MONGO_URI);
   console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline.bold);
-}
+};
