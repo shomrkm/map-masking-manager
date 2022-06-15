cad69c103fcbd07015904afb3c6ccfe2d25b2f99
s_murakami <s_murakami@geot.jp>
2022-06-15 09:08:52 +0900

    feat: Add Link element.

---
+++ frontend/src/components/Elements/Link/Link.tsx
@@ -0,0 +1,9 @@
+import { Link as RouterLink, LinkProps } from 'react-router-dom';
+
+export const Link = ({ className, children, ...props }: LinkProps) => {
+  return (
+    <RouterLink className={`text-indigo-600 hover:text-indigo-900 ${className}`} {...props}>
+      {children}
+    </RouterLink>
+  );
+};
--- frontend/src/components/Elements/index.ts
+++ frontend/src/components/Elements/index.ts
@@ -1,3 +1,4 @@
 export * from './Spinner';
 export * from './Button';
 export * from './Table';
+export * from './Link';
