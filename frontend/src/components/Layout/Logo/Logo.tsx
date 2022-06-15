86f31ee9d08591365a5570c4bfae4af805362316
s_murakami <s_murakami@geot.jp>
2022-06-15 09:08:21 +0900

    refact: Change the application name.

--- frontend/src/components/Layout/Logo/Logo.tsx
+++ frontend/src/components/Layout/Logo/Logo.tsx
@@ -5,8 +5,8 @@ import logo from '@/assets/logo.svg';
 export const Logo = () => {
   return (
     <Link className="flex items-center text-white" to="/">
-      <img className="w-auto h-8" src={logo} alt="Shomrkm App" />
-      <span className="text-xl font-semibold text-white">Shomrkm App</span>
+      <img className="w-auto h-8" src={logo} alt="Map Tasking Manager" />
+      <span className="text-xl font-semibold text-white">Map Tasking Manager</span>
     </Link>
   );
 };
--- frontend/src/features/misc/routes/dashboard.tsx
+++ frontend/src/features/misc/routes/dashboard.tsx
@@ -2,7 +2,7 @@ export const Dashboard = () => {
   return (
     <>
       <h1 className="p-4 text-2xl">Dashboard</h1>
-      <div className="pl-2 text-base">Welcome to our application.</div>
+      <div className="pl-2 text-base">Welcome to Map Tasking Manager.</div>
     </>
   );
 };
