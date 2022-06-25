da506f5de4da463b5b9208f2c1db27a13e072a52
shomrkm <shochan1985@gmail.com>
2022-06-25 13:59:47 +0900

    feat: Add user CURD requests.

--- backend/controller/users.ts
+++ backend/controller/users.ts
@@ -1,9 +1,64 @@
 import { Request, Response, NextFunction } from 'express';
+import { asyncHandler } from '../middleware';
 import { AdvancedResponse } from '../types';
+import { User } from '../models';
 
 // @desc Get all users
 // @route GET /api/v1/users
 // @access Public
-export const getUsers = async (req: Request, res: AdvancedResponse, next: NextFunction) => {
-  res.status(200).json(res.advancedResults);
-};
+export const getUsers = asyncHandler(
+  async (req: Request, res: AdvancedResponse, next: NextFunction) => {
+    res.status(200).json(res.advancedResults);
+  }
+);
+
+// @desc      Get single user
+// @route     GET /api/v1/users/:id
+// @access    Private/Admin
+export const getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Create user
+// @route     POST /api/v1/users
+// @access    Private/Admin
+export const createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Update user
+// @route     PUT /api/v1/users/:id
+// @access    Private/Admin
+export const updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Delete user
+// @route     DELETE /api/v1/users/:id
+// @access    Private/Admin
+export const deleteUser = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- backend/routes/Users.ts
+++ backend/routes/Users.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { advancedResults } from '../middleware';
-import { getUsers } from '../controller/users';
+import { getUsers, getUser, createUser, updateUser, deleteUser } from '../controller/users';
 import { User } from '../models/Users';
 
 export const router = express.Router();
 
-router.route('/').get(advancedResults(User), getUsers);
+router.route('/').get(advancedResults(User), getUsers).post(createUser);
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
