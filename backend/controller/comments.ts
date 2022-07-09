105c14c1f2c4de2bbda2c9433f41938a452de386
shomrkm <shochan1985@gmail.com>
2022-07-09 11:10:32 +0900

    feat: Add create new comment request.

--- backend/controller/comments.ts
+++ backend/controller/comments.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { Comment } from '../models/Comments';
+import { Task } from '../models/Tasks';
 import { asyncHandler } from '../middleware';
 import { ErrorResponse } from '../utils';
 
@@ -31,4 +32,26 @@ export const getComment = asyncHandler(async (req: Request, res: Response, next:
   }
 
   res.status(200).json({ success: true, data: comment });
+});
+
+// @desc      Add comment
+// @route     POST /api/v1/tasks/:taskid/comments
+// @access    Private
+export const addComment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.params);
+  req.body.task = req.params.taskid;
+  req.body.user = req.user.id;
+
+  const task = await Task.findById(req.params.taskid);
+
+  if (!task) {
+    return next(new ErrorResponse(`No task with the id of ${req.params.taskid}`, 404));
+  }
+
+  const comment = await Comment.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: comment,
+  });
 });
--- backend/routes/Comments.ts
+++ backend/routes/Comments.ts
@@ -1,16 +1,19 @@
 import express from 'express';
-import { advancedResults } from '../middleware';
-import { getComments, getComment } from '../controller/comments';
+import { advancedResults, protect, authorize } from '../middleware';
+import { getComments, getComment, addComment } from '../controller/comments';
 import { Comment } from '../models/Comments';
 
 export const router = express.Router({ mergeParams: true });
 
-router.route('/').get(
-  advancedResults(Comment, [
-    { path: 'task', select: 'title' },
-    { path: 'user', select: 'name' },
-  ]),
-  getComments
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Comment, [
+      { path: 'task', select: 'title' },
+      { path: 'user', select: 'name' },
+    ]),
+    getComments
+  )
+  .post(protect, authorize('publisher', 'admin'), addComment);
 
 router.route('/:id').get(getComment);
