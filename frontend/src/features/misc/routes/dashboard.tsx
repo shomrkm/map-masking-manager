8b952890696ccfa0db9fd9a25befb0bc583a526f
shomrkm <shochan1985@gmail.com>
2022-08-12 08:04:51 +0900

    feat: Display the username in the dashboard page.

--- frontend/src/features/misc/routes/dashboard.tsx
+++ frontend/src/features/misc/routes/dashboard.tsx
@@ -1,10 +1,14 @@
 import { ContentLayout } from '@/components/Layout';
+import { useAuth } from '@/hooks/useAuth';
 
 export const Dashboard = () => {
+  const { user } = useAuth();
+
   return (
     <ContentLayout title="Dashboard">
       <div className="flex-col">
-        <div className="pl-2 text-base">Welcome to Map Tasking Manager.</div>
+        <p className="pl-2 text-base">{`Hello ${user?.name}`}</p>
+        <p className="pl-2 text-base">Welcome to Map Tasking Manager.</p>
       </div>
     </ContentLayout>
   );
