94e0803b27f1265935f2ba0df3d8d0fbad2bfc06
shomrkm <shochan@gmail.com>
2022-08-12 08:39:34 +0900

    Fix: Fix bug displaying undefined as username in the dashborad.

--- frontend/src/features/auth/api/getUser.ts
+++ frontend/src/features/auth/api/getUser.ts
@@ -2,5 +2,5 @@ import { User } from '@/features/users/types';
 import { axios } from '@/lib/axios';
 
 export const getUser = async (): Promise<User | undefined> => {
-  return await axios.get('/auth/me');
+  return (await axios.get('/auth/me')).data;
 };
