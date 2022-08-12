35ad73a8f44601854e771c1b089daa944c472d33
shomrkm <shochan1985@gmail.com>
2022-08-12 17:31:35 +0900

    feat: Change background color in the login page.

--- frontend/src/features/auth/routes/Login.tsx
+++ frontend/src/features/auth/routes/Login.tsx
@@ -7,8 +7,8 @@ export const Login = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center w-full h-screen bg-gradient-to-tr from-gray-300 to-gray-100">
-        <div className="flex overflow-hidden z-10 flex-col md:flex-row items-center mx-3 md:mx-5 lg:mx-0 w-full sm:w-1/2 md:w-9/12 bg-blue-600 bg-center bg-cover bg-login rounded shadow-md">
+      <div className="flex justify-center items-center w-full h-screen bg-gradient-to-tr from-gray-800 to-gray-300">
+        <div className="flex overflow-hidden z-10 flex-col md:flex-row items-center mx-3 md:mx-5 lg:mx-0 w-full sm:w-1/2 md:w-2/3 bg-blue-600 bg-center bg-cover bg-login rounded shadow-md">
           <div className="flex flex-col justify-center items-center w-full md:w-1/2 bg-blue-600 bg-opacity-25">
             <h1 className="my-2 md:my-0 text-3xl md:text-4xl font-extrabold text-white">
               MapTaskingManager
