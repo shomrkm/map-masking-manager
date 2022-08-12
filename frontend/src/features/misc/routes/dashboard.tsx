cc395eb5df25bd97e2bb812355978166a457dea7
shomrkm <shochan1985@gmail.com>
2022-08-12 09:03:14 +0900

    feat: Display user profile in the dashboard.

--- frontend/src/features/misc/routes/dashboard.tsx
+++ frontend/src/features/misc/routes/dashboard.tsx
@@ -7,8 +7,18 @@ export const Dashboard = () => {
   return (
     <ContentLayout title="Dashboard">
       <div className="flex-col">
-        <p className="pl-2 text-base">{`Hello ${user?.name}`}</p>
-        <p className="pl-2 text-base">Welcome to Map Tasking Manager.</p>
+        <div className="flex">
+          <div className="mr-2 text-base text-gray-700">Welcome</div>
+          <div className="font-bold text-gray-700">{user?.name}</div>
+        </div>
+        <div className="flex">
+          <div className="mr-2 text-base text-gray-700">Your role is </div>
+          <div className="font-bold text-gray-700">{user?.role}</div>
+        </div>
+        <div className="flex">
+          <div className="mr-2 text-base text-gray-700">Your level is </div>
+          <div className="font-bold text-gray-700">{user?.level}</div>
+        </div>
       </div>
     </ContentLayout>
   );
