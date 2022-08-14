15eb4590aaa9fab9f3d8938b49e4b96d71ae5b43
shomrkm <shochan1985@gmail.com>
2022-08-14 10:00:13 +0900

    feat: Add createTask api hook.

---
+++ frontend/src/features/tasks/api/createTask.ts
@@ -0,0 +1,61 @@
+import { useMutation } from 'react-query';
+
+import { axios } from '@/lib/axios';
+import { MutationConfig, queryClient } from '@/lib/react-query';
+import { useNotificationStore } from '@/stores/notifications';
+
+import { Task } from '../types';
+
+export type CreateTaskDTO = {
+  data: {
+    title: string;
+    description: string;
+    detail: string;
+    area: {
+      type: 'Polygon';
+      coordinates: number[][][];
+    };
+    workflow: string;
+    target: ('road' | 'map' | 'poi')[];
+    level: 'expert' | 'intermediate' | 'biginner';
+    priority: 'high' | 'normal' | 'low';
+  };
+};
+
+export const createTask = async ({ data }: CreateTaskDTO): Promise<Task> => {
+  return (await axios.post('/tasks', data)).data;
+};
+
+type UseCreateTaskOptions = {
+  config?: MutationConfig<typeof createTask>;
+};
+
+export const useCreateTask = ({ config }: UseCreateTaskOptions = {}) => {
+  const { addNotification } = useNotificationStore();
+
+  return useMutation({
+    onMutate: async (newTask: CreateTaskDTO) => {
+      // Cancel query to avoid rewriting optimistic update by old data.
+      await queryClient.cancelQueries('tasks');
+      // Get cache and optimistic update
+      const previousTasks = queryClient.getQueryData<Task[]>('tasks');
+      queryClient.setQueryData('tasks', [...(previousTasks || []), newTask]);
+      // Send context to onError
+      return { previousTasks };
+    },
+    onError: (_, __, context: any) => {
+      if (context?.previousTasks) {
+        queryClient.setQueryData('tasks', context.previousTasks);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries('tasks');
+      addNotification({
+        type: 'success',
+        title: 'Task Created',
+      });
+    },
+    ...config,
+    mutationFn: createTask,
+  });
+};
