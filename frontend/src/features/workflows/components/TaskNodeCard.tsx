bfab6b560ffa2e6546496b967179b80894c40c09
shomrkm <shochan1985@gmail.com>
2022-08-18 09:31:25 +0900

    feat: Change TaskNode layout.

--- frontend/src/features/workflows/components/TaskNodeCard.tsx
+++ frontend/src/features/workflows/components/TaskNodeCard.tsx
@@ -1,3 +1,4 @@
+import { PencilAltIcon } from '@heroicons/react/outline';
 import React from 'react';
 import { Node, Handle, Position } from 'react-flow-renderer';
 import { Link } from 'react-router-dom';
@@ -19,12 +20,13 @@ export const TaskNodeCard = ({ data }: TaskData) => {
   return (
     <>
       <Handle type="target" position={Position.Top} />
-      <div className="flex-col justify-center items-center p-3 bg-gray-100 rounded-md border border-gray-800 border-solid">
-        <div className="flex justify-center items-center">
+      <div className="flex-col justify-center items-center p-3 bg-gray-100 rounded-md border border-gray-200 border-solid shadow-sm">
+        <div className="flex justify-center items-center pb-1 border-b-2 border-solid">
+          <PencilAltIcon className="p-1 mr-2 w-6 h-6 text-gray-800 bg-gray-300 rounded-full" />
           <Link to={`../../tasks/${_id}`} className="pr-2 text-xs text-blue-700">{`#${id}`}</Link>
-          <p className="text-xs text-gray-700">{title}</p>
+          <p className="text-sm text-gray-700">{title}</p>
         </div>
-        <div className="flex justify-center items-center mt-1">
+        <div className="flex justify-center items-center mt-2">
           <TaskStatusBadge status={status} size="sm" />
         </div>
       </div>
