ed354551b02c8a55ec96e575890d3d36859815dc
shomrkm <shochan1985@gmail.com>
2022-08-21 16:25:50 +0900

    feat: Add TaskRepository Interface.

---
+++ backend/src/application/repositories/ITaskRepository.ts
@@ -0,0 +1,8 @@
+import { Task } from '@/domain/Task';
+
+export abstract class ITaskRepository {
+  // abstract findAll(): Promise<Array<Task>>;
+  // abstract find(id: string): Promise<Task>;
+  abstract save(task: Task): Promise<Task>;
+  // abstract delete(task: Task): Promise<Task>;
+}
