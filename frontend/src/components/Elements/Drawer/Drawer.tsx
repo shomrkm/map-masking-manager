ba9ffd5a3675c4dfe3d2d1aed5707febb45487c4
shomrkm <shochan1985@gmail.com>
2022-08-24 13:38:39 +0900

    fix: Modify Drawer copoment.

--- frontend/src/components/Elements/Drawer/Drawer.tsx
+++ frontend/src/components/Elements/Drawer/Drawer.tsx
@@ -39,42 +39,44 @@ export const Drawer = ({
       >
         <div className="overflow-hidden absolute inset-0">
           <Dialog.Overlay className="absolute inset-0" />
-          <Transition.Child
-            as={React.Fragment}
-            enter="transform transition ease-in-out duration-300 sm:duration-300"
-            enterFrom="translate-x-full"
-            enterTo="translate-x-0"
-            leave="transform transtion ease-in-out duration-300 sm:duration-300"
-            leaveFrom="translate-x-0"
-            leaveTo="translate-x-full"
-          >
-            <div className={clsx('w-screen', sizes[size])}>
-              <div className="flex flex-col h-full bg-white divide-y divide-gray-200 shadow-xl">
-                <div className="flex overflow-y-scroll flex-col flex-1 py-6 min-h-0">
-                  <div className="px-4 sm:px-6">
-                    <div className="flex justify-between items-start">
-                      <Dialog.Title className="text-lg font-medium text-gray-900">
-                        {title}
-                      </Dialog.Title>
-                      <div className="flex items-center ml-3 h-7">
-                        <button
-                          className="text-gray-400 hover:text-gray-500 bg-white rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-                          onClick={onClose}
-                        >
-                          <span className="sr-only">Close panel</span>
-                          <XIcon className="w-6 h-6" aria-hidden="true" />
-                        </button>
+          <div className="flex fixed inset-y-0 right-0 pl-10 max-w-full">
+            <Transition.Child
+              as={React.Fragment}
+              enter="transform transition ease-in-out duration-300 sm:duration-300"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform transtion ease-in-out duration-300 sm:duration-300"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
+            >
+              <div className={clsx('w-screen', sizes[size])}>
+                <div className="flex flex-col h-full bg-white divide-y divide-gray-200 shadow-xl">
+                  <div className="flex overflow-y-scroll flex-col flex-1 py-6 min-h-0">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex justify-between items-start">
+                        <Dialog.Title className="text-lg font-medium text-gray-900">
+                          {title}
+                        </Dialog.Title>
+                        <div className="flex items-center ml-3 h-7">
+                          <button
+                            className="text-gray-400 hover:text-gray-500 bg-white rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+                            onClick={onClose}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XIcon className="w-6 h-6" aria-hidden="true" />
+                          </button>
+                        </div>
                       </div>
                     </div>
+                    <div className="relative flex-1 px-4 sm:px-6 mt-6">{children}</div>
                   </div>
-                  <div className="relative flex-1 px-4 sm:px-6 mt-6">{children}</div>
-                </div>
-                <div className="flex flex-shrink-0 justify-end py-4 px-4 space-x-2">
-                  {renderFooter()}
+                  <div className="flex flex-shrink-0 justify-end py-4 px-4 space-x-2">
+                    {renderFooter()}
+                  </div>
                 </div>
               </div>
-            </div>
-          </Transition.Child>
+            </Transition.Child>
+          </div>
         </div>
       </Dialog>
     </Transition.Root>
