fcb9b3d2bb95e5f529dec55f4d7fca082c0a7ba6
shomrkm <shochan1985@gmail.com>
2022-09-15 09:04:58 +0900

    feat: Add authorization for delete task.

--- frontend/src/features/workflows/components/TaskNodeCard.tsx
+++ frontend/src/features/workflows/components/TaskNodeCard.tsx
@@ -1,11 +1,12 @@
 import { PencilAltIcon } from '@heroicons/react/outline';
 import clsx from 'clsx';
-import React, { CSSProperties } from 'react';
+import { CSSProperties } from 'react';
 import { Node, Handle, Position } from 'react-flow-renderer';
 import { Link } from 'react-router-dom';
 
 import { LevelBadge, TaskStatusBadge } from '@/components/Elements';
 import { Task } from '@/features/tasks/types';
+import { Authorization, ROLES } from '@/lib/authorization';
 
 import { RadialProgress } from '../../../components/Elements/RadialProgress/RadialProgress';
 
@@ -43,7 +44,9 @@ export const TaskNodeCard = ({ data }: TaskData) => {
           <PencilAltIcon className="p-1 mr-2 w-6 h-6 text-gray-800 bg-gray-300 rounded-full" />
           <Link to={`../../tasks/${_id}`} className="pr-2 text-sm text-blue-700">{`#${id}`}</Link>
           <p className="flex-1 text-sm text-gray-700">{title}</p>
-          <ModifyTaskNodeMenu taskId={_id} />
+          <Authorization allowedRoles={[ROLES.admin]}>
+            <ModifyTaskNodeMenu taskId={_id} />
+          </Authorization>
         </div>
         <div className="border-b border-gray-400 border-solid" />
         <div className="flex justify-center items-center p-3 space-x-2">
@@ -53,7 +56,7 @@ export const TaskNodeCard = ({ data }: TaskData) => {
               <TaskStatusBadge status={status} size="sm" />
             </div>
             <div className="flex items-center space-x-2">
-              <p>Level:</p>
+              <p>Level :</p>
               <LevelBadge level={level} size="sm" />
             </div>
           </div>
