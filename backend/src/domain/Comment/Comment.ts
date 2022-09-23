5f2f34fc6cc73e8cd3462e002924578d118f6950
shomrkm <shochan1985@gmail.com>
2022-09-23 17:10:38 +0900

    refact: Add Comment domain classes.

---
+++ backend/src/domain/Comment/Comment.ts
@@ -0,0 +1,51 @@
+import moment, { Moment } from 'moment-timezone';
+
+import { Text } from './entities';
+
+type Params = {
+  id?: string | null;
+  task: string;
+  user: string;
+  text: string;
+  createdAt?: Date | null;
+};
+
+export class Comment {
+  private _id: string | null;
+  private _task: string;
+  private _user: string;
+  private _text: Text;
+  private _createdAt: moment.Moment;
+
+  constructor({ task, user, text, id = null, createdAt = null }: Params) {
+    this._id = id;
+    this._task = task;
+    this._user = user;
+    this._text = new Text(text);
+    this._createdAt = createdAt ? moment(createdAt) : moment(new Date());
+  }
+
+  get id(): string | null {
+    return this._id;
+  }
+
+  set id(id: string | null) {
+    if (id) this._id = id;
+  }
+
+  get task(): string {
+    return this._task;
+  }
+
+  get user(): string {
+    return this._user;
+  }
+
+  get text(): string {
+    return this._text.get();
+  }
+
+  get createdAt(): moment.Moment {
+    return this._createdAt;
+  }
+}
---
+++ backend/src/domain/Comment/entities/Text.ts
@@ -0,0 +1,11 @@
+import { ErrorResponse } from '@/interface/controller/errorResponse';
+import { ValueObject } from '../../base/ValueObject';
+
+export class Text extends ValueObject<string> {
+  constructor(text: string) {
+    if (text.length > 500) {
+      throw new ErrorResponse('Text can not be more than 500 characters', 400);
+    }
+    super(text);
+  }
+}
