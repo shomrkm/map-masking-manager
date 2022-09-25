499c7f1d5611c5e0581e5e25c943de54b9086528
shomrkm <shochan1985@gmail.com>
2022-09-26 08:26:51 +0900

    refact: Remove unused methods.

--- backend/src/interface/controller/comments.ts
+++ backend/src/interface/controller/comments.ts
@@ -4,42 +4,6 @@ import { Task } from '@/infrastructure/database/models/Tasks';
 import { ErrorResponse } from './errorResponse';
 import { asyncHandler } from './asyncHandler';
 
-// @desc Get all comments
-// @route GET /api/v1/comments
-// @route GET /api/v1/tasks/:taskId/comments
-// @access Public
-export const getComments = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  if (req.params.taskid) {
-    const comments = await Comment.find({ task: req.params.taskid }).populate({
-      path: 'user',
-      select: 'name avatar',
-    });
-
-    return res.status(200).json({
-      success: true,
-      count: comments.length,
-      data: comments,
-    });
-  }
-
-  res.status(200).json(res.advancedResults);
-});
-
-// @desc Get single comment
-// @route GET /api/v1/comments/:id
-// @access Public
-export const getComment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  const comment = await Comment.findById(req.params.id).populate({
-    path: 'user',
-    select: 'name avatar',
-  });
-  if (!comment) {
-    return next(new ErrorResponse(`Comment not found with id of ${req.params.id}`, 404));
-  }
-
-  res.status(200).json({ success: true, data: comment });
-});
-
 // @desc      Add comment
 // @route     POST /api/v1/tasks/:id/comments
 // @access    Private
