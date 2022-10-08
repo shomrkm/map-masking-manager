686ec971ec0819addcd9779ad397c7278180e52f
shomrkm <shochan1985@gmail.com>
2022-10-08 16:37:50 +0900

    refact: Modify User domain.

--- backend/src/domain/User/User.ts
+++ backend/src/domain/User/User.ts
@@ -1,4 +1,6 @@
 import moment from 'moment-timezone';
+import { Level } from '../Task/entities';
+import { Role } from './entities/Role';
 
 type Params = {
   name: string;
@@ -15,8 +17,8 @@ type Params = {
 export class User {
   private _name: string;
   private _email: string;
-  private _role: string;
-  private _level: string;
+  private _role: Role;
+  private _level: Level;
   private _avatar: string;
   private _password: string;
   private _resetPasswordToken: string | null;
@@ -36,8 +38,8 @@ export class User {
   }: Params) {
     this._name = name;
     this._email = email;
-    this._role = role;
-    this._level = level;
+    this._role = new Role(role);
+    this._level = new Level(level);
     this._avatar = avatar;
     this._password = password;
     this._resetPasswordToken = resetPasswordToken ?? null;
---
+++ backend/src/domain/User/entities/Role.ts
@@ -0,0 +1,18 @@
+import { ErrorResponse } from '@/interface/controller/errorResponse';
+import { ValueObject } from '../../base/ValueObject';
+
+export const roleTypes = ['admin', 'publisher', 'mapper'] as const;
+export type RoleType = typeof roleTypes[number];
+
+function isRole(role: string): role is RoleType {
+  return roleTypes.includes(role as RoleType);
+}
+
+export class Role extends ValueObject<RoleType> {
+  constructor(role: string) {
+    if (!isRole(role)) {
+      throw new ErrorResponse(`Role must be ${roleTypes.join(',')}`, 400);
+    }
+    super(role);
+  }
+}
