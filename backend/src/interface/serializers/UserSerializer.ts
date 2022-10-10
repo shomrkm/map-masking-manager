ebe62a6bbe16082904b643e3abd865471945d925
shomrkm <shochan1985@gmail.com>
2022-10-10 18:04:02 +0900

    refact: Create UserSerializer.

---
+++ backend/src/interface/serializers/UserSerializer.ts
@@ -0,0 +1,22 @@
+import { User } from '@/domain/User';
+
+const serializeSingleUser = (user: User) => {
+  return {
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    level: user.level,
+    avatar: user.avatar,
+    createdAt: user.createdAt,
+  };
+};
+
+export class UserSerializer {
+  public serializeUser(user: User) {
+    return serializeSingleUser(user);
+  }
+  public serializeUsers(users: User[]) {
+    return users.map(serializeSingleUser);
+  }
+}
