3038ce6fc636add74b30461f6f37446f14a8cf45
shomrkm <shochan1985@gmail.com>
2022-10-10 18:01:12 +0900

    fix: Modify User domain.

--- backend/src/domain/User/User.ts
+++ backend/src/domain/User/User.ts
@@ -12,9 +12,11 @@ type Params = {
   resetPasswordToken?: string;
   resetPasswordExpire?: Date;
   createdAt?: Date | null;
+  id?: string;
 };
 
 export class User {
+  private _id: string | null;
   private _name: string;
   private _email: string;
   private _role: Role;
@@ -35,7 +37,9 @@ export class User {
     resetPasswordToken = undefined,
     resetPasswordExpire = undefined,
     createdAt = undefined,
+    id = undefined,
   }: Params) {
+    this._id = id ?? null;
     this._name = name;
     this._email = email;
     this._role = new Role(role);
@@ -45,5 +49,82 @@ export class User {
     this._resetPasswordToken = resetPasswordToken ?? null;
     this._resetPasswordExpire = resetPasswordExpire ? moment(resetPasswordExpire) : null;
     this._createdAt = createdAt ? moment(createdAt) : moment(new Date());
+  }
+
+  get id(): string | null {
+    return this._id;
+  }
+
+  set id(id: string | null) {
+    if (id) this._id = id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  // TODO: Create ValueObject for validate email
+  get email(): string {
+    return this._email;
+  }
+
+  set email(email: string) {
+    this._email = email;
+  }
+
+  get role(): string {
+    return this._role.get();
+  }
+
+  set role(role: string) {
+    this._role = new Role(role);
+  }
+
+  get level(): string {
+    return this._level.get();
+  }
+
+  set level(level: string) {
+    this._level = new Level(level);
+  }
+
+  get avatar(): string {
+    return this._avatar;
+  }
+
+  set avatar(avatar: string) {
+    this._avatar = avatar;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(password: string) {
+    this._password = password;
+  }
+
+  get resetPasswordToken(): string | null {
+    return this._resetPasswordToken;
+  }
+
+  set resetPasswordToken(token: string | null) {
+    if (token) this._resetPasswordToken = token;
+  }
+
+  get resetPasswordExpired(): moment.Moment | null {
+    return this._resetPasswordExpire;
+  }
+
+  set resetPasswordExpired(date: moment.Moment | null) {
+    if (date) this._resetPasswordExpire = moment(date);
+  }
+
+  get createdAt(): moment.Moment {
+    return this._createdAt;
   }
 }
