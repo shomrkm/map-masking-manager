3f978c6017891eec876dad0f0b75294e3f88a273
shomrkm <shochan1985@gmail.com>
2022-10-16 17:27:06 +0900

    refact: Implement findAll method in the UserRepository.

--- backend/src/application/repositories/IUserRepository.ts
+++ backend/src/application/repositories/IUserRepository.ts
@@ -1,5 +1,6 @@
 import { User } from '@/domain/User';
 
 export interface IUserRepository {
+  findAll(): Promise<User[]>;
   find(id: string): Promise<User>;
 }
--- backend/src/interface/repositories/UserRepository.ts
+++ backend/src/interface/repositories/UserRepository.ts
@@ -8,6 +8,25 @@ export class UserRepository implements IUserRepository {
     this.dbConnection = dbConnection;
   }
 
+  public async findAll(): Promise<User[]> {
+    const userDtos = await this.dbConnection.findAllUsers();
+    return userDtos.map(
+      (userDto) =>
+        new User({
+          id: userDto._id,
+          name: userDto.name,
+          email: userDto.email,
+          role: userDto.role,
+          level: userDto.level,
+          avatar: userDto.avatar,
+          password: userDto.password,
+          resetPasswordToken: userDto.resetPasswordToken ?? undefined,
+          resetPasswordExpire: userDto.resetPasswordExpire ?? undefined,
+          createdAt: userDto.createdAt,
+        })
+    );
+  }
+
   public async find(id: string): Promise<User> {
     const userDto = await this.dbConnection.findUserById(id);
     const user = new User({
