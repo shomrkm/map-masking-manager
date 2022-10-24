895cb842173e20022de3a567a970e124e7090475
shomrkm <shochan1985@gmail.com>
2022-10-24 09:07:16 +0900

    feat: Add authority management to TaskCustomEdge to modify workflow.

--- frontend/src/features/workflows/components/TaskCustomEdge.tsx
+++ frontend/src/features/workflows/components/TaskCustomEdge.tsx
@@ -2,6 +2,8 @@ import { XCircleIcon } from '@heroicons/react/outline';
 import React, { CSSProperties } from 'react';
 import { Position, getBezierPath } from 'react-flow-renderer';
 
+import { Authorization, ROLES } from '@/lib/authorization';
+
 const FOREIGN_OBJECT_SIZE = 20;
 
 type CustomTaskEdgeProps = {
@@ -46,21 +48,23 @@ export const TaskCustomEdge = ({
         d={edgePath}
         markerEnd={markerEnd}
       />
-      <foreignObject
-        width={FOREIGN_OBJECT_SIZE}
-        height={FOREIGN_OBJECT_SIZE}
-        x={(sourceX + targetX) / 2 - FOREIGN_OBJECT_SIZE / 2}
-        y={(sourceY + targetY) / 2 - FOREIGN_OBJECT_SIZE / 2}
-        className="flex justify-center items-center w-10 h-10 bg-transparent"
-        requiredExtensions="http://www.w3.org/1999/xhtml"
-      >
-        <button
-          className="flex justify-center items-center"
-          onClick={() => console.log('clicked!')}
+      <Authorization allowedRoles={[ROLES.admin]}>
+        <foreignObject
+          width={FOREIGN_OBJECT_SIZE}
+          height={FOREIGN_OBJECT_SIZE}
+          x={(sourceX + targetX) / 2 - FOREIGN_OBJECT_SIZE / 2}
+          y={(sourceY + targetY) / 2 - FOREIGN_OBJECT_SIZE / 2}
+          className="flex justify-center items-center w-10 h-10 bg-transparent"
+          requiredExtensions="http://www.w3.org/1999/xhtml"
         >
-          <XCircleIcon className="w-5 h-5 text-gray-400 hover:text-red-400 rounded-full opacity-20 hover:opacity-80 cursor-pointer" />
-        </button>
-      </foreignObject>
+          <button
+            className="flex justify-center items-center"
+            onClick={() => console.log('clicked!')}
+          >
+            <XCircleIcon className="w-5 h-5 text-gray-400 hover:text-red-400 rounded-full opacity-20 hover:opacity-80 cursor-pointer" />
+          </button>
+        </foreignObject>
+      </Authorization>
     </>
   );
 };
