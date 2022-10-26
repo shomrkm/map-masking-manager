474ba9a000a1aadb8cef2e2e719d1a3bd9a18811
shomrkm <shochan1985@gmail.com>
2022-10-26 09:16:22 +0900

    feat: Display tasks in the workflow page.

--- frontend/src/features/workflows/routes/Workflow.tsx
+++ frontend/src/features/workflows/routes/Workflow.tsx
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom';
 
-import { Spinner } from '@/components/Elements';
+import { LevelBadge, Link, Spinner, StatusBadge, Table } from '@/components/Elements';
 import { ContentLayout } from '@/components/Layout';
+import { useTasksForWorkflow } from '@/features/tasks/api/getTasksForWorkFlow';
+import { Task } from '@/features/tasks/types';
 
 import { useWorkflow } from '../api/getWorkflow';
 import { TaskWorkflowPanel } from '../components/TaskWorkflowPanel';
@@ -10,6 +12,7 @@ export const Workflow = () => {
   const { workflowId } = useParams();
 
   const workflowQuery = useWorkflow({ workflowId: workflowId as string });
+  const taskQuery = useTasksForWorkflow({ workflowId: workflowId as string });
 
   if (workflowQuery.isLoading) {
     return (
@@ -30,6 +33,41 @@ export const Workflow = () => {
           <div className="flex-1 space-y-4">
             <h2 className="text-xl font-bold text-gray-700 prose">Description</h2>
             <p className="text-base">{workflowQuery.data.description}</p>
+            <h2 className="text-xl font-bold text-gray-700 prose">Tasks</h2>
+            <div className="mr-4">
+              {taskQuery.data && (
+                <Table<Task>
+                  data={taskQuery.data}
+                  columns={[
+                    {
+                      title: 'ID',
+                      field: 'id',
+                      Cell({ entry: { _id, id } }) {
+                        return <Link to={`../../tasks/${_id}`}>{id}</Link>;
+                      },
+                    },
+                    {
+                      title: 'Title',
+                      field: 'title',
+                    },
+                    {
+                      title: 'Status',
+                      field: 'status',
+                      Cell({ entry: { status } }) {
+                        return <StatusBadge status={status} size="sm" />;
+                      },
+                    },
+                    {
+                      title: 'Level',
+                      field: 'level',
+                      Cell({ entry: { level } }) {
+                        return <LevelBadge level={level} size="sm" />;
+                      },
+                    },
+                  ]}
+                />
+              )}
+            </div>
           </div>
           <div className="hidden lg:flex">
             <div className="flex justify-center items-center w-[700px] h-[750px] rounded-md border border-gray-300 border-solid shadow-sm">
