0d06a275467420d170a487f88f428789ee178440
shomrkm <shochan1985@gmail.com>
2022-11-10 16:36:11 +0900

    chore: Update BASE_URL.

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://164.92.154.252';
-export const API_URL = `${BASE_URL}/api/v1/`;
+export const BASE_URL = 'http://164.92.154.252/api';
+export const API_URL = `${BASE_URL}/v1/`;
