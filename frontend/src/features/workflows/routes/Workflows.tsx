4ce197251625dfc20119b925e25f2a0c90611e75
shomrkm <shochan1985@gmail.com>
2022-11-18 08:58:50 +0900

    [wip] feat: Add Button to create Workflow.

--- frontend/src/features/workflows/routes/Workflows.tsx
+++ frontend/src/features/workflows/routes/Workflows.tsx
@@ -1,5 +1,7 @@
 import { Spinner, Table, Link, StatusBadge } from '@/components/Elements';
 import { ContentLayout } from '@/components/Layout';
+import { CreateTaskButton } from '@/features/tasks/components/CreateTaskButton';
+import { Authorization, ROLES } from '@/lib/authorization';
 
 import { useWorkflows } from '../api/getWorkflows';
 import { Workflow } from '../types';
@@ -22,40 +24,48 @@ export const Workflows = () => {
   return (
     <>
       <ContentLayout title="Workflows">
-        <Table<Workflow>
-          data={data}
-          columns={[
-            {
-              title: 'ID',
-              field: 'id',
-              Cell({ entry: { _id, id } }) {
-                return <Link to={`./${_id}`}>{id}</Link>;
+        <div className="flex-col">
+          <Authorization allowedRoles={[ROLES.admin]}>
+            <div className="flex pb-4">
+              {/* TODO: Change below to CreateWorkflowButton */}
+              <CreateTaskButton buttonTitle="Create" />
+            </div>
+          </Authorization>
+          <Table<Workflow>
+            data={data}
+            columns={[
+              {
+                title: 'ID',
+                field: 'id',
+                Cell({ entry: { _id, id } }) {
+                  return <Link to={`./${_id}`}>{id}</Link>;
+                },
               },
-            },
-            {
-              title: 'Status',
-              field: 'status',
-              Cell({ entry: { status } }) {
-                return <StatusBadge status={status} size="sm" />;
+              {
+                title: 'Status',
+                field: 'status',
+                Cell({ entry: { status } }) {
+                  return <StatusBadge status={status} size="sm" />;
+                },
               },
-            },
-            {
-              title: 'Title',
-              field: 'title',
-            },
-            {
-              title: 'Description',
-              field: 'description',
-            },
-            {
-              title: 'Created by',
-              field: 'createUser',
-              Cell({ entry: { createUser } }) {
-                return <p>{createUser.name}</p>;
+              {
+                title: 'Title',
+                field: 'title',
               },
-            },
-          ]}
-        />
+              {
+                title: 'Description',
+                field: 'description',
+              },
+              {
+                title: 'Created by',
+                field: 'createUser',
+                Cell({ entry: { createUser } }) {
+                  return <p>{createUser.name}</p>;
+                },
+              },
+            ]}
+          />
+        </div>
       </ContentLayout>
     </>
   );
