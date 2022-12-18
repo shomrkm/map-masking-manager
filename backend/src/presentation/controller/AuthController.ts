c82ddbd3d3d335f9bc13ba6c3ef24fd60aa75cda
shomrkm <shochan1985@gmail.com>
2022-12-18 11:21:35 +0900

    refact: Refactoring a register user request.

--- backend/src/presentation/controller/AuthController.ts
+++ backend/src/presentation/controller/AuthController.ts
@@ -3,12 +3,37 @@ import { UserRepository } from '@/infrastructure/repositories/UserRepository';
 import { UserSerializer } from '../serializers/UserSerializer';
 import { ErrorResponse } from '@/shared/core/utils';
 import { Login } from '@/application/usecases/Auth/login';
+import { Register } from '@/application/usecases/Auth/register';
 
 export class AuthController {
   constructor(
     private readonly userRepository = new UserRepository(),
     private readonly userSerializer = new UserSerializer()
   ) {}
+
+  public async register(req: Request, res: Response) {
+    const { name, email, password, role, level } = req.body;
+    if (!name || !email || !password || !role || !level) {
+      throw new ErrorResponse('Please provide an name, email, role, level and password', 400);
+    }
+
+    const registerUser = new Register(this.userRepository);
+    const { user, token, cookieOptions } = await registerUser.execute(
+      name,
+      email,
+      role,
+      level,
+      password
+    );
+    res
+      .status(200)
+      .cookie('token', token, cookieOptions)
+      .json({
+        success: true,
+        data: this.userSerializer.serializeUser(user),
+        token,
+      });
+  }
 
   public async login(req: Request, res: Response) {
     const { email, password } = req.body;
--- backend/src/presentation/router/Auth.ts
+++ backend/src/presentation/router/Auth.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import { protect } from '@/shared/core/middleware/authorization';
-import { register, logout, getMe, updatePassword, updateDetails } from '../controller/auth';
+import { logout, getMe, updatePassword, updateDetails } from '../controller/auth';
 import { AuthController } from '../controller/AuthController';
 
 const authController = new AuthController();
 
 export const router = express.Router();
 
-router.post('/register', register);
+// @route POST /api/v1/auth/register (Public)
+router.post('/register', authController.register.bind(authController));
+// @route POST /api/v1/auth/login (Public)
 router.post('/login', authController.login.bind(authController));
+
 router.post('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/updatepassword', protect, updatePassword);
