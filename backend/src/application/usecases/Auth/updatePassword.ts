6491dd3e86da38441faaf3dd7c1f00b9991e53de
shomrkm <shochan1985@gmail.com>
2022-12-23 18:24:29 +0900

    feat: Add Update Password usecase.

---
+++ backend/src/application/usecases/Auth/updatePassword.ts
@@ -0,0 +1,35 @@
+import { CookieOptions } from 'express';
+import { User } from '@/domain/entities';
+import { ErrorResponse } from '@/shared/core/utils';
+import { IUserRepository } from '../../repositories/IUserRepository';
+import { createCookieOptions } from './utils/createCookieOptions';
+
+type ReturnType = {
+  user: User;
+  token: string;
+  cookieOptions: CookieOptions;
+};
+
+export class UpdatePassword {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  public async execute(
+    id: string,
+    currentPassword: string,
+    newPassword: string
+  ): Promise<ReturnType> {
+    const user = await this.userRepository.find(id, { selectPassword: true });
+    console.log(user);
+    const isMatch = await user.matchPassword(currentPassword);
+    if (!isMatch) {
+      throw new ErrorResponse('Invalid credentials', 401);
+    }
+
+    await user.setNewPassword(newPassword);
+    const updatedUser = await this.userRepository.save(user);
+
+    const token = user.getSignedJwtToken();
+    const cookieOptions = createCookieOptions();
+    return { user: updatedUser, token, cookieOptions };
+  }
+}
