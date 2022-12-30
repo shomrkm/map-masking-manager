237bad12a15e90886e3c14348152b3c8b7dbab59
shomrkm <shochan1985@gmail.com>
2022-12-30 11:16:29 +0900

    fix: Remove unused codes.

--- backend/src/domain/ValueObjects/Title.ts
+++ backend/src/domain/ValueObjects/Title.ts
@@ -1,9 +1,6 @@
 import { StringValueObject } from '@/shared/domain/StringValueObject';
 import { ErrorResponse } from '@/shared/core/utils';
 
-const EMAIL_PATTERN =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 export class Title extends StringValueObject {
   constructor(title: string) {
     if (title.length > 50) {
