3a4f3888f6982578ce0ab9feba3839c7320d1f8b
shomrkm <shochan1985@gmail.com>
2022-12-30 11:00:40 +0900

    feat: Add a Email ValueObject.

--- backend/src/domain/ValueObjects/Email.ts
+++ backend/src/domain/ValueObjects/Email.ts
@@ -4,11 +4,11 @@ import { ErrorResponse } from '@/shared/core/utils';
 const EMAIL_PATTERN =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export class Title extends StringValueObject {
-  constructor(title: string) {
-    if (title.length > 50) {
-      throw new ErrorResponse('Title can not be more than 50 characters', 400);
+export class Email extends StringValueObject {
+  constructor(email: string) {
+    if (!email.match(EMAIL_PATTERN)) {
+      throw new ErrorResponse(`Email '${email}' is invalid`, 400);
     }
-    super(title);
+    super(email);
   }
 }
---
+++ backend/src/domain/ValueObjects/__tests__/Email.test.ts
@@ -0,0 +1,20 @@
+import { ErrorResponse } from '@/shared/core/utils';
+import { Email } from '../Email';
+
+describe('Email ValueObject test', () => {
+  test('should not throw error if email is valid', () => {
+    expect(() => new Email('shomrkm@test.com')).not.toThrowError();
+  });
+
+  test('should throw error if email is invalid', () => {
+    expect(() => new Email('testmail')).toThrowError(
+      new ErrorResponse("Email 'testmail' is invalid", 400)
+    );
+    expect(() => new Email('(test@test.com')).toThrowError(
+      new ErrorResponse("Email '(test@test.com' is invalid", 400)
+    );
+    expect(() => new Email('test@test@com')).toThrowError(
+      new ErrorResponse("Email 'test@test@com' is invalid", 400)
+    );
+  });
+});
