df2e4285d3f4247b6300b0b9251aa80547cbf3a9
shomrkm <shochan1985@gmail.com>
2023-01-18 18:16:32 +0900

    refact: (backend) Remove comments.

--- backend/src/application/usecases/Task/DeleteTask.ts
+++ backend/src/application/usecases/Task/DeleteTask.ts
@@ -16,28 +16,6 @@ export class DeleteTask {
       throw new ErrorResponse(`Task was not found with id of ${taskId}`, 404);
     }
 
-    // if (task.next.length) {
-    //   task.next.forEach(async (nextId) => {
-    //     const nextTask = await this.taskRepository.findById(nextId);
-    //     if (!nextTask) {
-    //       throw new ErrorResponse(`Task was not found with id of ${nextId}`, 404);
-    //     }
-    //     nextTask.removePreviousTask(taskId);
-    //     await this.taskRepository.save(nextTask);
-    //   });
-    // }
-    // if (task.previous.length) {
-    //   task.previous.forEach(async (prevId) => {
-    //     const prevTask = await this.taskRepository.findById(prevId);
-    //     if (!prevTask) {
-    //       throw new ErrorResponse(`Task was not found with id of ${prevId}`, 404);
-    //     }
-    //     prevTask.removeNextTask(taskId);
-    //     console.log(prevTask);
-    //     await this.taskRepository.save(prevTask);
-    //   });
-    // }
-
     return await this.taskRepository.delete(taskId);
   }
 }
