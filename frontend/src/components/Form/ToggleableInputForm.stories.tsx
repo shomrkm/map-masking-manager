e0be0c108d606f91aad60a29315fc80f166e2ba4
shomrkm <shochan1985@gmail.com>
2023-03-25 15:15:39 +0900

    test: Update ToggleableInputForm Story.

--- frontend/src/components/Form/ToggleableInputForm.stories.tsx
+++ frontend/src/components/Form/ToggleableInputForm.stories.tsx
@@ -3,13 +3,13 @@ import { Meta, Story } from '@storybook/react';
 import { ToggleableInputForm, ToggleableInputFormProps } from './ToggleableInputForm';
 
 const MyToggleableInputForm = (props: ToggleableInputFormProps) => (
-  <div className="w-40">
+  <div className="w-44">
     <ToggleableInputForm {...props} />
   </div>
 );
 
 const meta: Meta = {
-  title: 'Components/Form',
+  title: 'Components/Form/ToggleableInputForm',
   component: MyToggleableInputForm,
   parameters: {
     controls: { expand: true },
@@ -19,8 +19,30 @@ export default meta;
 
 const Template: Story<ToggleableInputFormProps> = (props) => <MyToggleableInputForm {...props} />;
 
-export const ToggleableInput = Template.bind({});
-ToggleableInput.args = {
-  defaultValue: 'test',
+export const Small = Template.bind({});
+Small.args = {
+  size: 'sm',
+  defaultValue: 'Small Text',
+  onSubmit: (value: string) => console.log(value),
+};
+
+export const Base = Template.bind({});
+Base.args = {
+  size: 'base',
+  defaultValue: 'Base Text',
+  onSubmit: (value: string) => console.log(value),
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'lg',
+  defaultValue: 'Large Text',
+  onSubmit: (value: string) => console.log(value),
+};
+
+export const XLarge = Template.bind({});
+XLarge.args = {
+  size: 'xl',
+  defaultValue: 'XLarge Text',
   onSubmit: (value: string) => console.log(value),
 };
