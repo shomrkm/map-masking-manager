0964ae832cd9927d9c93a68cbd426454a69b5848
shormkm <shochan1985@gmail.com>
2023-05-19 09:21:02 +0900

    feat: Update ToggleableInputForm component.

--- frontend/src/components/Form/ToggleableInputForm.tsx
+++ frontend/src/components/Form/ToggleableInputForm.tsx
@@ -38,6 +38,11 @@ export const ToggleableInputForm: FC<ToggleableInputFormProps> = ({
     setValue(e.target.value);
   };
 
+  const handleCancelClick = () => {
+    setValue(defaultValue);
+    setIsEditing(false);
+  };
+
   const handleSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
@@ -59,27 +64,28 @@ export const ToggleableInputForm: FC<ToggleableInputFormProps> = ({
               className={`block py-2 px-3 w-full text-${size} placeholder-gray-400 rounded-md border border-gray-300 focus:border-blue-500 focus:ring-blue-500 shadow-sm appearance-none focus:outline-none`}
             />
             <div className="flex absolute right-3 -bottom-7 gap-2">
-            <button
-              type="submit"
-              value="Submit"
-              className="flex ustify-center items-center bg-gray-200 hover:bg-blue-200 rounded-sm"
+              <button
+                type="submit"
+                value="Submit"
+                className="flex justify-center items-center bg-gray-200 hover:bg-blue-200 rounded-sm"
               >
-              <CheckIcon className="w-5 h-5 text-gray-400 hover:text-gray-600" />
-            </button>
-            <button
-              type="button"
-              value="Cancel"
-              className="flex justify-center items-center bg-gray-200 hover:bg-blue-200 rounded-sm"
+                <CheckIcon className="w-5 h-5 text-gray-400 hover:text-gray-600" />
+              </button>
+              <button
+                type="button"
+                value="Cancel"
+                className="flex justify-center items-center bg-gray-200 hover:bg-red-200 rounded-sm"
+                onClick={handleCancelClick}
               >
-              <XIcon className="w-5 h-5 text-gray-400 hover:text-gray-600" />
-            </button>
+                <XIcon className="w-5 h-5 text-gray-400 hover:text-gray-600" />
+              </button>
             </div>
           </div>
         ) : (
           <div className="flex items-center">
             <input
               type="text"
-              defaultValue={value}
+              value={value}
               onClick={handleInputClick}
               className={clsx(
                 'block py-2 px-3 w-full rounded-md hover:border hover:border-gray-300 hover:shadow-sm appearance-none',
